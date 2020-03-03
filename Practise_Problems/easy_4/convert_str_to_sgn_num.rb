cff0709caec71969ef2f048aead3bb1b6c721d63
Richard Morris <rwmorris.bjj@gmail.com>
2020-03-03 21:31:04 +0000

    added solutions to practise problems

--- Practise_Problems/easy_4/convert_str_to_sgn_num.rb
+++ Practise_Problems/easy_4/convert_str_to_sgn_num.rb
@@ -1,3 +1,13 @@
+# PROBLEM
+# --------------------
+
+# Write a method that takes a String of digits, and returns the appropriate number as an integer. 
+# The String may have a leading + or - sign; if the first character is a +, your method should 
+# return a positive number; if it is a -, your method should return a negative number. 
+# If no sign is given, you should return a positive number.
+
+
+
 INTEGERS = { '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
 			'6' => 6, '7' => 7, '8' => 8, '9' => 9 }
 
@@ -16,13 +26,13 @@ end
 	
 def string_to_signed_integer(str)
 	if str[0] == '+'
-		string_to_integer(str[1..-1])
+		puts +string_to_integer(str[1..-1])
 	elsif str[0] == '-'
-		string_to_integer(str[1..-1])
+		puts -string_to_integer(str[1..-1])
     else
-    	string_to_integer(str)
+    	puts string_to_integer(str)
 	end
 end 
 
-string_to_signed_integer('-1234')
+string_to_signed_integer('+570')
 

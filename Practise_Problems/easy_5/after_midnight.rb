f82e0c4511e470cf9fc7736bf29278f556b16ca9
Richard Morris <rwmorris.bjj@gmail.com>
2020-03-06 20:46:49 +0000

    edit to after_midnight

--- Practise_Problems/easy_5/after_midnight.rb
+++ Practise_Problems/easy_5/after_midnight.rb
@@ -23,28 +23,28 @@
 #        should work with any integer input 
 #		 You may not use ruby's Date and Time classes.
 
-# Algorithim 
-Initalize local variable HOURS_IN_DAY and assign it with the value 
-of 24 as an integer object.
-Initalize local variable MINUTES_PER_DAY and assign it with the value
-of 60 as an integer object.
-Initalize the variable MINUTES_PER_DAY and assign it the return value
-of HOURS_IN_DAY multiplied by MINUTES_IN_HOUR.
-Define the method time_of_day and intialize tha paramter int.
-Initalize the variable time_remainder and assign it
-the value of the time remaining after the int modulo has been caluculated.
-using MINUTES_PER_DAY.
-E.g 1440 cannot go into 120, therefore 120 is returned as 1440 goes into
-120 0 times. 
-two local variables are intialized within the method hours and minutes.
-They are assigned the return values of the divmod method when invoked on
-the time_remainder variable.
-hours = 120 / 60 = 2 
-minutes = 0 / 60 = 0
-The format method is then invoked and passes the arguments (hours, minutes)
-to the format_string. In this case 2 and 0. 
-This then returns a string with the arguments formatted correctly
-to represent a digital clock time. 
+# # Algorithim 
+# Initalize local variable HOURS_IN_DAY and assign it with the value 
+# of 24 as an integer object.
+# Initalize local variable MINUTES_PER_DAY and assign it with the value
+# of 60 as an integer object.
+# Initalize the variable MINUTES_PER_DAY and assign it the return value
+# of HOURS_IN_DAY multiplied by MINUTES_IN_HOUR.
+# Define the method time_of_day and intialize tha paramter int.
+# Initalize the variable time_remainder and assign it
+# the value of the time remaining after the int modulo has been caluculated.
+# using MINUTES_PER_DAY.
+# E.g 1440 cannot go into 120, therefore 120 is returned as 1440 goes into
+# 120 0 times. 
+# two local variables are intialized within the method hours and minutes.
+# They are assigned the return values of the divmod method when invoked on
+# the time_remainder variable.
+# hours = 120 / 60 = 2 
+# minutes = 0 / 60 = 0
+# The format method is then invoked and passes the arguments (hours, minutes)
+# to the format_string. In this case 2 and 0. 
+# This then returns a string with the arguments formatted correctly
+# to represent a digital clock time. 
 
 
 HOURS_IN_DAY = 24

578605e479c6ed19d36cac9323897b4102f39ab4
Richard Morris <rwmorris.bjj@gmail.com>
2020-03-17 19:59:51 +0000

    edit on cap_word.rb

--- Practise_Problems/easy_7/cap_word.rb
+++ Practise_Problems/easy_7/cap_word.rb
@@ -1,5 +1,5 @@
 # solution:
-ef word_cap(str)
+def word_cap(str)
 	arr = str.split
 
 	arr.map! do |word|
@@ -14,3 +14,4 @@ end
 p word_cap('four score and seven') == 'Four Score And Seven'
 p word_cap('the javaScript language') == 'The Javascript Language'
 p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+

4598d8aa0bb7291d47dfac077d48cd2454d1b3be
Richard Morris <rwmorris.bjj@gmail.com>
2020-03-17 19:57:11 +0000

    added solution to cap_word

---
+++ Practise_Problems/easy_7/cap_word.rb
@@ -0,0 +1,16 @@
+# solution:
+ef word_cap(str)
+	arr = str.split
+
+	arr.map! do |word|
+		word.capitalize
+	end
+
+	arr.join(' ')
+
+end
+
+
+p word_cap('four score and seven') == 'Four Score And Seven'
+p word_cap('the javaScript language') == 'The Javascript Language'
+p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'

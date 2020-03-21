8bda44a4ea26fb8bce1a53b6f01bf7dd02a61559
Richard Morris <rwmorris.bjj@gmail.com>
2020-03-21 17:10:01 +0000

    futher edits to welcome_stranger.rb

--- Practise_Problems/easy_9/welcome_stranger.rb
+++ Practise_Problems/easy_9/welcome_stranger.rb
@@ -10,11 +10,11 @@ greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
 # Further exploration:
 def greetings_1(arr, hsh)
 
-	<<-text 
-	"Hello, #{arr.join(' ')}!
-	Nice to have a #{hsh[:title]} #{hsh[:occupation]} around."
+	puts <<-text 
+	Hello, #{arr.join(' ')}!
+	Nice to have a #{hsh[:title]} #{hsh[:occupation]} around.
 	text
 
 end
 
-puts greetings_1(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
+greetings_1(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })

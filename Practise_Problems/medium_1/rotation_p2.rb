3d600675d2faede3f85c967d06e21df718cb184e
Richard Morris <rwmorris.bjj@gmail.com>
2020-03-23 15:13:01 +0000

    removed whitespace

--- Practise_Problems/medium_1/rotation_p2.rb
+++ Practise_Problems/medium_1/rotation_p2.rb
@@ -1,21 +1,13 @@
 
 # Solution:
 def rotate_rightmost_digits(num, places)
-
 	num_str = num.to_s
-
 	index = num.digits.length - places 
-
 	section_to_rotate = num_str.slice!(index..-1)
-
 	slice = section_to_rotate[1..-1]
-
 	slice << section_to_rotate[0]
-
 	num_str << slice
-
 	num_str.to_i
-
 end
 
 p rotate_rightmost_digits(735291, 1) == 735291

2baee55279863999161a9aed05ea61c1b75434c1
Richard Morris <rwmorris.bjj@gmail.com>
2020-03-30 12:15:30 +0100

    added further exploration solution to code

--- Practise_Problems/medium_2/letter_case_percentage.rb
+++ Practise_Problems/medium_2/letter_case_percentage.rb
@@ -19,7 +19,7 @@ def letter_percentages(str)
 		if result == result.to_i
 			v = result.to_i
 		else
-			v = result
+			v = result.round(2)
 		end
 	end
 
@@ -30,4 +30,4 @@ end
 p letter_percentages('abCdef 123') == { lowercase: 50, uppercase: 10, neither: 40 }
 p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25 }
 p letter_percentages('123') == { lowercase: 0, uppercase: 0, neither: 100 }
-
+p letter_percentages('abcdefGHI')
